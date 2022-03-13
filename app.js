4528ead7befa60d8ff0da9a894def665faa05a9b
abdelrahmannehab <100984030+abdelrahmannehab@users.noreply.github.com>
2022-03-13 11:30:22 +0200

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { json } = require('body-parser');
 const express = require('express');
 const dbConnection = require('./config/db');
 const app = express();
-const userRoutes = require('./src/companies/routes/company.routes');
+//const userRoutes = require('./src/companies/routes/company.routes');
 const port = 5000
 
 
@@ -12,6 +12,6 @@ app.use(express.json());
 
 
 dbConnection();
-app.use(userRoutes);
+//app.use(userRoutes);
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
