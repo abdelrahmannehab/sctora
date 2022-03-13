f68b75c1ce09b72aeda89082fe815d7fb8bf67fd
abdelrahmannehab <100984030+abdelrahmannehab@users.noreply.github.com>
2022-03-13 11:46:17 +0200

    serves

--- config/db.js
+++ config/db.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const dbConnection = () => mongoose.connect('mongodb+srv://abdelrahman:Mm01274098137@cluster0.olod5.mongodb.net/sctora').then((result)=>{
-    console.log("db connected");
-}).catch((err)=>{
-    console.log("db not connected" , err);
-})
+
 
 
 
--- src/companies/Schema/company.schema.js
+++ src/companies/Schema/company.schema.js
@@ -3,8 +3,8 @@ const  mongoose = require('mongoose');
 const { Schema } = mongoose
 const companySchema = new Schema({
     companyName: {type:String},
-    email : String,
-    //password 
+    email : {type: String},
+    password : {type: String}
 
 },{
     timestamps: true
