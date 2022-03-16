e54c61609a1ba70b0f00d2381a5653c21a629f9c
abdelrahmannehab <100984030+abdelrahmannehab@users.noreply.github.com>
2022-03-16 13:21:16 +0200

    goo

--- config/db.js
+++ config/db.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
 
+const dbConnection = () => mongoose.connect('mongodb+srv://abdelrahman:Mm01274098137@cluster0.olod5.mongodb.net/sctora').then((result)=>{
+    console.log("db connected");
+}).catch((err)=>{
+    console.log("db not connected" , err);
+})
+
 
 
 
