d2dacc320aef2c6e9d1f6edd85a719d71e3d8f1b
abdelrahmannehab <100984030+abdelrahmannehab@users.noreply.github.com>
2022-06-18 17:44:52 +0200

    she

--- src/actors/Schema/actor.schema.js
+++ src/actors/Schema/actor.schema.js
@@ -4,7 +4,7 @@ const{Schema} = mongoose
 
 const actorSchema = new Schema ({
     UserName: {type: String , required: true},
-    ActorEmail: {type: String , required: true},
+    ActorEmail: {type: String , required: true, unique: true},
     Gender: {type: String , required: true},
     Age: {type: String , required: true},
     Weight: {type: String , required: true},
--- src/companies/Schema/company.schema.js
+++ src/companies/Schema/company.schema.js
@@ -5,8 +5,9 @@ const companySchema = new Schema({
     CompanyName: {type:String , required : true},
     CompanyManagerName : {type:String , required: true},
     CompanyAddress : {type:String , required: true},
-    CompanyEmail : {type: String, required: true},
+    CompanyEmail : {type: String, required: true,unique:true},
     CompanyPassword : {type: String, required: true},
+    
 
 },{
     timestamps: true
