4acfb415410c8d07d883b315fb01d61f55bf5bc1
abdelrahmannehab <100984030+abdelrahmannehab@users.noreply.github.com>
2022-07-01 23:25:54 +0200

    2

--- middlewear/auth.js
+++ middlewear/auth.js
@@ -16,7 +16,7 @@ const auth = ()=>{
             const decoded = jwt.verify(token, process.env.secretKey)
             const actor = await Actor.findOne({_id:decoded.id}).select("-ActorPassword")
             if (!actor) {
-                res.json({message:"in-valid token data"})
+                res.json(error.message)
             }else{
                 req.actor = actor;
                 next()
