17660c44e1478eafd645e2ba6b8db886b6d62631
abdelrahmannehab <100984030+abdelrahmannehab@users.noreply.github.com>
2022-07-01 21:03:38 +0200

    ed

--- src/actors/routes/actor.routes.js
+++ src/actors/routes/actor.routes.js
@@ -12,7 +12,7 @@ const router = require('express').Router()
 router.get("/actors",getAllActorsHandlr);
 router.get("/actor/:id",getAllActorsHandlr);
 //router.post("/actors",addActorHandlr);
-router.post("/actors",handlerValidation(ActorRegisterValidators),ActorRegistration);
+router.post("/actors/register",handlerValidation(ActorRegisterValidators),ActorRegistration);
 router.post("/actorsLogin",handlerValidation(ActorLoginValidators),ActorLogin);
 router.patch("/actors/:id", handlerValidation(ActorUpdateValidators),updateActorHandlr);
 
